2726d91c6f8587abdedaf449c7e570a02f30c20c
mattiac02 <91368154+mattiac02@users.noreply.github.com>
2021-09-25 10:52:17 +0200

    Update ordina.js

--- ordina.js
+++ ordina.js
@@ -1,15 +1,9 @@
 $(document).ready(
     function(){
         $("#order").click(function(){
-            //gets all div children of the main div, this returns a jQuery array with the elements
-            var divs = $('#main');
-            //sorts the divs based on the id attribute
-            var ordered = divs.sort(function(a, b) {
-            return $(a).attr('id') - $(b).attr('id');
-            alert(ordered);
+            
         });
-            //empty the main div and re-append the ordered divs
-            //$('#main').empty().append(ordered);
+       
         });
     }
   );
